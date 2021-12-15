d769726c83ac22c7842d0ef5c7161963395ca15f
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-15 15:06:08 +0100

    Testing INSERT into

--- app/users/register.php
+++ app/users/register.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../autoload.php';
 
 // In this file we register a new user.
 
-
+//to upload an avatar-img to the avatar-folder (located in database)
 if (isset($_FILES['avatar'])) {
     $avatar = $_FILES['avatar'];
     $destination =  __DIR__ . '/../database/avatars' . date("y-m-d") . $avatar['name'];
@@ -14,6 +14,17 @@ if (isset($_FILES['avatar'])) {
     $message = 'The file was successfully uploaded!';
 }
 
+//to insert username in database
 
+if (isset($_POST['name'])) {
+    $username = trim($_POST['name']);
+    $email = $_POST['email'];
+    $password = $_FILES['password'];
+    $imgurl = $_POST['avatar'];
+
+    $database = new PDO('sqlite:database.db');
+
+    $database->exec("INSERT INTO users (username, email, password, image_url) VALUES ('$username', '$email', '$password','$imgurl')");
+}
 
 redirect('/');
