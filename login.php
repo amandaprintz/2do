2f3602ea0ab9e6c6e200088c2930e1d02c34a374
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-15 10:50:45 +0100

    Editing form

--- login.php
+++ login.php
@@ -2,19 +2,19 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 <article>
-    <h1>Login</h1>
+    <h1>Login</h1> <br>
 
     <form action="app/users/login.php" method="post">
         <div class="mb-3">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text">Please provide the your email address.</small>
+            <input class="form-control" type="email" name="email" id="email" placeholder="email@email.com" required>
+            <small class="form-text">Please write your email address.</small>
         </div>
 
         <div class="mb-3">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text">Please provide the your password (passphrase).</small>
+            <small class="form-text">Please write your password.</small>
         </div>
 
         <button type="submit" class="btn btn-primary">Login</button>
