6dbc26977d129318d5c2ce1d1ff2a9d564dfa5b4
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-15 12:29:07 +0100

    Structured forms

--- register.php
+++ register.php
@@ -4,16 +4,17 @@
 
 <article>
     <h1>Create an account</h1> <br>
-
-    <div class="mb-3">
-        <label for="email">Username</label>
-        <input class="form-control" type="name" name="name" id="name" placeholder="" required>
-        <small class="form-text">Choose your username.</small>
-    </div>
     <form action="app/users/register.php" method="post">
+
+        <div class="mb-3">
+            <label for="email">Username</label>
+            <input class="form-control" type="name" name="name" id="name" placeholder="AllanOpio" required>
+            <small class="form-text">Choose your username.</small>
+        </div>
+
         <div class="mb-3">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="" required>
+            <input class="form-control" type="email" name="email" id="email" placeholder="contact@opm.com" required>
             <small class="form-text">Register your email address.</small>
         </div>
 
@@ -24,29 +25,27 @@
         </div>
 
 
-
         <div class="mb-3">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" id="password" required>
             <small class="form-text"> Choose your password.</small>
         </div>
 
-    </form>
-    <br>
-
-    <form action="/" method="post" enctype="multipart/form-data">
-
-        <div>
-            <label for="avatar">Choose your avatar image to upload</label> <br>
-            <input type="file" accept=".jpg, .jpeg, .png" name="avatar" id="avatar" required>
-        </div>
-
 
         <br>
-        <form method="get" action="/signup.php">
-            <button type="submit" class="btn btn-secondary">Create an account</button>
-        </form>
 
+        <form action="/" method="post" enctype="multipart/form-data">
+
+            <div>
+                <label for="avatar">Choose your avatar image to upload</label> <br>
+                <input type="file" accept=".jpg, .jpeg, .png" name="avatar" id="avatar" required>
+            </div>
+            <br>
+
+            <button type="submit" class="btn btn-secondary">Create an account</button>
+
+        </form>
+    </form>
 </article>
 
 
