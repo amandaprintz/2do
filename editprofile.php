caa0037d6232fa2319473dc50f9852f8a1945cad
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-20 10:51:32 +0100

    Added edit profile

--- editprofile.php
+++ editprofile.php
@@ -1,1 +1,17 @@
-<?php
+<?php require __DIR__ . '/app/autoload.php'; ?>
+<?php require __DIR__ . '/views/header.php'; ?>
+
+<h1>Edit your profile</h1> <br>
+<form action="/app/users/register.php" method="post" enctype="multipart/form-data">
+
+    <div class="mb-3">
+        <label for="avatar">Edit profile-picture</label>
+        <br>
+        <input type="file" accept=".jpg, .jpeg, .png" name="avatar" id="avatar" required>
+        <small class="form-text"> Formats accepted: Jpg, Jpeg or Png. </small>
+    </div>
+    <button type="submit" class="btn btn-secondary">Edit profile</button>
+
+</form>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
+<?php require __DIR__ . '/app/users/images.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+
 
 
 
