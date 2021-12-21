7aaf3f98dec406ea508603da88801774cccc6b4e
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-21 10:20:33 +0100

    Updated nav

--- views/navigation.php
+++ views/navigation.php
@@ -10,12 +10,21 @@
             <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/about.php' ? 'active' : ''; ?>" href="/about.php">About</a>
         </li>
 
+
+
         <li class="nav-item">
             <?php if (isset($_SESSION['user'])) : ?>
-                <a class="nav-link" href="/app/users/logout.php">Logout</a>
-            <?php else : ?>
-                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
-            <?php endif; ?>
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/profile.php' ? 'active' : ''; ?>" href="/profile.php">Edit profile</a>
         </li>
+
+        <a class="nav-link" href="/app/users/logout.php">Logout</a>
+
+
+    <?php else : ?>
+        <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+    <?php endif; ?>
+    </li>
     </ul>
 </nav>
