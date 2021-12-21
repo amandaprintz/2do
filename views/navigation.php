4b0ece6d6d366e404352f3bceeb9aa384203a84c
amandaprintz <fannyamandakarlsson@gmail.com>
2021-12-21 11:01:49 +0100

    Commit errors

--- views/navigation.php
+++ views/navigation.php
@@ -18,7 +18,8 @@
         <a class="nav-link" href="/app/users/logout.php">Logout</a>
 
     <?php else : ?>
-        <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a><?php endif; ?>
+        <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+    <?php endif; ?>
     </li>
     </ul>
 </nav>
