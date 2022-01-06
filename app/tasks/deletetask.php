03b866f4cfa056556fca98755645f0ee49335633
amandaprintz <fannyamandakarlsson@gmail.com>
2022-01-06 15:19:32 +0100

    Started on delete task

--- app/tasks/deletetask.php
+++ app/tasks/deletetask.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+/* Logic for deleting a task */
 
+if (isset($_POST['id'])) {
+    $taskID = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $insertSQL = $database->prepare('DELETE FROM tasks WHERE id = :id');
+    $insertSQL->bindParam(':id', $taskID, PDO::PARAM_INT);
+    $insertSQL->execute();
 
+    $insertSQL = $database->prepare('DELETE FROM tasks WHERE list_id = :id');
+    $insertSQL->bindParam(':id', $taskID, PDO::PARAM_INT);
+}
 
 redirect('/');
