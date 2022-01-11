d6224c8e71f92d8afa723d0cae91ad641ad183bb
amandaprintz <fannyamandakarlsson@gmail.com>
2022-01-11 15:00:31 +0100

    Corrected error

--- views/navigation.php
+++ views/navigation.php
@@ -10,29 +10,28 @@
 
     <!--     These links are visible if you are LOGGED IN. -->
     <ul class="navbar-nav">
-        <li class="nav-item">
-            <?php if (isset($_SESSION['user'])) : ?>
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/mylists.php' ? 'active' : ''; ?>" href="/mylists.php">My Lists</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/profile.php' ? 'active' : ''; ?>" href="/profile.php">Edit profile</a>
-        </li>
-        <li> <a class="nav-link" href="/app/users/logout.php">Logout</a>
-        </li>
-        <!--     These links are visible if you are LOGGED OUT. -->
-    <?php else : ?>
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/register.php' ? 'active' : ''; ?>" href="register.php">Create an account</a>
-        </li>
+        <?php if (isset($_SESSION['user'])) : ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/mylists.php' ? 'active' : ''; ?>" href="/mylists.php">My Lists</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/profile.php' ? 'active' : ''; ?>" href="/profile.php">Edit profile</a>
+            </li>
+            <li> <a class="nav-link" href="/app/users/logout.php">Logout</a>
+            </li>
+            <!--     These links are visible if you are LOGGED OUT. -->
+        <?php else : ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/login.php' ? 'active' : ''; ?>" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php echo $_SERVER['SCRIPT_NAME'] === '/register.php' ? 'active' : ''; ?>" href="register.php">Create an account</a>
+            </li>
 
-    <?php endif; ?>
+        <?php endif; ?>
     </ul>
 
 
